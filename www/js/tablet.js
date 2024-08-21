3c13e8a04a72bb099818314d0368e3a8c87620e9
BarbourSmith <BarbourSmith@users.noreply.github.com>
2024-08-21 13:46:14 -0700

    Bump version number

--- www/js/tablet.js
+++ www/js/tablet.js
@@ -6,7 +6,7 @@ var sndok = true
 
 var lastHeartBeatTime = new Date().getTime();
 
-var versionNumber = 0.82
+var versionNumber = 0.83
 
 //Print the version number to the console
 let msgWindow = document.getElementById('messages')
